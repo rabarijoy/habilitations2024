be8a518abfb0e23681efaa824983fe2ec33d79aa
rabarijoy <rabarijoy@icloud.com>
2025-06-01 05:51:35 -0700

    Fichiers tests unitaires

---
+++ habilitations2024.Tests/DeveloppeurAccessTests.cs
@@ -0,0 +1,29 @@
+﻿using habilitations2024.dal;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace habilitations2024.Tests
+{
+    [TestClass]
+    public class DeveloppeurAccessTests
+    {
+        [TestMethod]
+        public void TestGetLesDeveloppeursAvecProfil()
+        {
+            // Arrange
+            var liste = DeveloppeurAccess.GetLesDeveloppeurs("DEV");
+
+            // Assert
+            Assert.AreEqual(3, liste.Count); // adapte ce nombre selon tes données
+        }
+
+        [TestMethod]
+        public void TestGetLesDeveloppeursSansProfil()
+        {
+            // Arrange
+            var liste = DeveloppeurAccess.GetLesDeveloppeurs("");
+
+            // Assert
+            Assert.IsTrue(liste.Count > 0); // ou le nombre attendu exact
+        }
+    }
+}
